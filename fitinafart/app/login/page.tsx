c54ee723316fcbdaf12f0554aa68b08108f3ebbb
Tristan Duun <trisduu@proton.me>
2025-12-05 19:38:34 +0100

    Removed logo from login page

--- fitinafart/app/login/page.tsx
+++ fitinafart/app/login/page.tsx
@@ -1,10 +1,9 @@
 import LoginForm from "./login-form";
 
 export default function Login() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <img src="/logo.png" alt="FitInAFart" width={180} height={140} className="mb-4" />
-      <LoginForm />
-    </div>
-  );
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center">
+            <LoginForm />
+        </div>
+    );
 }
